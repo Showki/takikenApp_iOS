39b4aeece9ae9bfe3e66ea23225035a90fb85cdb
Showki <ering.scissorman@gmail.com>
2016-04-12 19:43:42 +0900

    Initial Commit

---
+++ takikenApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  takikenApp
+//
+//  Created by Nicdang on 2016/04/12.
+//  Copyright © 2016年 fksk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ takikenAppTests/takikenAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  takikenAppTests.swift
+//  takikenAppTests
+//
+//  Created by Nicdang on 2016/04/12.
+//  Copyright © 2016年 fksk. All rights reserved.
+//
+
+import XCTest
+@testable import takikenApp
+
+class takikenAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
